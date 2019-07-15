6ea4ad1526652cda0e55ba2d9e4e14d17a0eaae7
Sirius.Black <ofghrt@qq.com>
2019-07-15 11:18:53 +0800

    [修改]使用hash

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 Vue.use(Router);
 const router =  new Router({
-	mode: 'history',
+	mode: 'hash',
 	base: process.env.BASE_URL,
 	routes: [
 		{
